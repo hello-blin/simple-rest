4e272143c3cef745cdfd612d44325c39e19ff313
Blin Kukaj <kblin164@gmail.com>
2022-04-05 00:45:46 +0200

    Installed Lodash to use it for simplicity on the app and finished the whole endpoint to register users

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const { User, validate } = require("../models/User");
 const router = express.Router();
+const _ = require("lodash");
 
 router.post("/", async (req, res) => {
   let { error } = validate(req.body);
@@ -10,15 +11,11 @@ router.post("/", async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("Already Registered");
 
-  user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  });
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
 
   await user.save();
 
-  res.send(user);
+  res.send(_.pick(user, ["_id", "name", "email"]));
 });
 
 router.get("/", async (req, res) => {
