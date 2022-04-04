81c00c35ce1f3b274aba5bb216d21a4d55d64e23
Blin Kukaj <kblin164@gmail.com>
2022-04-05 00:55:48 +0200

    Hashing password through bcrypt

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const { User, validate } = require("../models/User");
 const router = express.Router();
+const bcrypt = require("bcrypt");
 const _ = require("lodash");
 
 router.post("/", async (req, res) => {
@@ -12,7 +13,8 @@ router.post("/", async (req, res) => {
   if (user) return res.status(400).send("Already Registered");
 
   user = new User(_.pick(req.body, ["name", "email", "password"]));
-
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
   await user.save();
 
   res.send(_.pick(user, ["_id", "name", "email"]));
